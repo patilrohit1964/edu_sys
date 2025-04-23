e13e08c235d40529d3140187bdaf743987694640
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-23 15:07:49 +0530

    add navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='bg-black'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
+
+
