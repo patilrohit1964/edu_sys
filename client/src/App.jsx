a26b5aaa19c7539a921e9eede0d4eb533a8c7724
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-23 18:26:16 +0530

    add home ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { Button } from './components/ui/button'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='bg-black'>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/' element=""></Route>
+        <Route path='/' element=""></Route>
+        <Route path='/' element=""></Route>
+        <Route path='/' element=""></Route>
+      </Routes>
     </div>
   )
 }
