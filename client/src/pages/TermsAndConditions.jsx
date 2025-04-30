2f5153b7ba7791f820ce8cdf4d0aa3291671f6ff
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-30 17:20:34 +0530

    added data

--- client/src/pages/TermsAndConditions.jsx
+++ client/src/pages/TermsAndConditions.jsx
@@ -72,6 +72,45 @@ const TermsAndConditions = () => {
                     You expressly agree that use of the Website, the Services, and the Content and Courseware are at your sole risk. We do not warrant that the Website or the Services or access to the Content and Courseware will be uninterrupted or error free; nor is there any warranty as to the results that may be obtained from the use of the Website, the Services or the Content and Courseware or as to the accuracy or reliability of any information provided through the Website, the Services, or the Content and Courseware. In no event will We or any person or entity involved in creating, producing, or distributing the Website, the Services, or the Content and Courseware be liable for any direct, indirect, incidental, special, or consequential damages arising out of the use of or inability to use the Website, the Services, or the Content and Courseware. The disclaimer of liability contained in this clause applies to any and all damages or injury caused by any failure of performance, error, omission, interruption, deletion, defect, delay in operation or transmission, computer virus, communication line failure, theft or destruction or unauthorized access to, alteration of, or use of records or any other material, whether for breach of contract, negligence, or under any other cause of action. You hereby specifically acknowledge that We are not liable for any defamatory, offensive, wrongful, or illegal conduct of third parties, or other users of the Website, the Services or the Content and Courseware and that the risk of damage or injury from the foregoing rests entirely with each user. You agree that Our liability or the liability of Our affiliates, directors, officers, employees, agents, and licensors, if any, arising out of any kind of legal claim (whether in contract, tort or otherwise) in any way connected with the Services or the Content and Courseware shall not exceed the fee you paid to Us for the particular training course.
                 </p>
             </div>
+            <div>
+                <h3 className='text-xl text-[#140342] font-semibold mb-8'>Term And Termination</h3>
+                <p className='text-gray-700 leading-8'>
+                    This Agreement Will Become Effective Upon Your Acceptance Of The Terms Of This Agreement By Your Clicking On The “I ACCEPT” Button And, Subject To The Terms And Conditions Of This Agreement, Will Remain In Effect Till You Maintain A Current, Fully Paid Up Online Participant Account, Or Until Terminated By Us, Whichever Is Earlier.
+                </p>
+                <p className='text-gray-700 leading-8'>
+                    We Reserve The Right To Terminate This Agreement And Block Your Access To The Content And Courseware With Immediate Effect By Sending A Written Notice Through Email To You To This Effect (“Immediate Termination Date”), If Such Termination Is Made As A Result Of Your Misrepresentation, Default, Misconduct, Or Breach Of Your Obligations Related To Or Under This Agreement (“Event Of Default”). On The Occurrence Of Any Event Of Default, We Shall Be Authorized To Exercise All The Rights And Remedies Under This Agreement Or Applicable Law Or Available In Equity To Seek Indemnification For Any Loss Or Claim Resulting From Any Such Event Of Default.
+                </p>
+            </div>
+            <div className='my-12'>
+                <h3 className='text-xl text-[#140342] font-semibold mb-8'>Indemnity</h3>
+                <p className='text-gray-700 leading-8'>
+                    You Agree To Indemnify And Hold Us, Our Contractors, Licensors, Directors, Officers, Employees, And Agents, Harmless From And Against Any And All Claims, Losses, Damages, Liabilities, And Expenses Including Attorneys’ Fees, Arising Out Of Your Unauthorized Use Of The Website, The Services, And The Content And Courseware Or Any Violation Or Breach Of This Agreement Or Any Provisions Hereof.
+                </p>
+            </div>
+            <div>
+                <h3 className='text-xl text-[#140342] font-semibold mb-8'>Waiver</h3>
+                <p className='text-gray-700 leading-8'>
+                    Neither Failure Nor Delay On The Part Of Any Party To Exercise Any Right, Remedy, Power, Or Privilege Hereunder Shall Operate As A Waiver Thereof, Or Of The Exercise Of Any Other Right, Remedy, Power, Or Privilege. No Term Of This Agreement Shall Be Deemed Waived, And No Breach Consented To, Unless Such Waiver Or Consent Shall Be In Writing And Signed By The Party Claimed To Have Waived Or Consented. No Waiver Of Any Rights Or Consent To Any Breaches Shall Constitute A Waiver Of Any Other Rights Or Consent To Any Other Breach.
+                </p>
+            </div>
+            <div className='my-12'>
+                <h3 className='text-xl text-[#140342] font-semibold mb-8'>Severability</h3>
+                <p className='text-gray-700 leading-8'>
+                    In The Event Any Provision Of This Agreement Is Held Invalid Or Unenforceable Under The Applicable Laws Of India, The Remaining Provisions Shall Continue In Full Force And Effect, And The Agreement Shall Be Deemed To Be Reformed By Replacing Such Invalidated Or Unenforceable Provision With A Valid And Enforceable Provision That Gives Effect As Closely As Possible To The Intentions Of The Parties As Expressed By The Invalidated Or Unenforceable Provision.
+                </p>
+            </div>
+            <div>
+                <h3 className='text-xl text-[#140342] font-semibold mb-8'>Governing Law And Jurisdiction</h3>
+                <p className='text-gray-700 leading-8'>For All Participants, This Agreement Shall Be Governed By And Construed In Accordance With The Laws Of India And The Courts In Uttar Pradesh, India Shall Have The Exclusive Jurisdiction Over Any Matter Relating To, In Connection With, Or Arising Out Of, This Agreement.</p>
+            </div>
+            <div className='my-12'>
+                <h3 className='text-xl text-[#140342] font-semibold mb-8'>Entire Agreement</h3>
+                <p className='text-gray-700 leading-8'>This Agreement, Along With The Privacy Policy, Refund Policy, Rescheduling Policy, Terms Of Use, And Any Additional Guidelines, Rules, And/or Disclaimers Posted On The Website Constitutes The Entire Agreement Governing Your Use Of Our Website And Supersedes Any Prior Agreements, If Any, Relating To Any Matter Dealt Within This Agreement.</p>
+            </div>
+            <div>
+                <h3 className='text-xl text-[#140342] font-semibold mb-8'>Job Guarantee Programs</h3>
+                <p className='text-gray-700 leading-8'>In case you have enrolled to our Job Guaranteed Program Please read our Job Guaranteed Programs terms and condition prior to your enrolment.</p>
+            </div>
         </div>
     )
 }
