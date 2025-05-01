b694e808ccb1fd449fec5a7439461625d9b35ae1
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-01 11:53:50 +0530

    privacy page starting

--- client/src/pages/PrivacyPolicy.jsx
+++ client/src/pages/PrivacyPolicy.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const PrivacyPolicy = () => {
   return (
-    <div className=''>PrivacyPolicy</div>
+    <div className='py-10 mx-auto max-w-7xl'>
+      <div>
+        <h1 className='text-4xl text-[#140342] font-bold'>Privacy policy.</h1>
+        <p className='text-gray-700 py-5 text-md font-bold'>Purpose</p>
+        <p className='text-gray-700 text-md'>Tech Minds Education (Tech Minds Education “we,” “us,” or “our”) is committed to protecting your privacy, we take data protection and privacy very seriously.</p>
+        <p className='mt-3 text-gray-700 leading-8'>
+          This Privacy Policy (“Policy”) describes how we collect, use, share and secure the personal information you provide when you visit the Websites owned and operated by us (listed below), and when you use our Service(s). It also describes your choices regarding use, access, withdrawal of consent (as per the terms hereof) and correction of your personal information. The use of information collected through our Service(s) shall be limited to the purpose of
+          providing the service that you have chosen. The capitalized terms used in this Policy but not defined herein shall have the same meaning as defined in our Terms and Conditions at (“Terms and Conditions ”). For the purpose of this Privacy Policy, sensitive personal data or information of a personal nature is as defined under applicable privacy laws. Please note that usage of the terms personal information/ personally identifiable information/ personal data, in this Privacy Policy includes sensitive personal data or
+          information, wherever appropriate and/or mandated under applicable privacy laws.
+        </p>
+      </div>
+    </div>
   )
 }
 
