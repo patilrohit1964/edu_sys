421d97b3e75c212cb923d9fba34951c0580d91b3
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-01 12:48:30 +0530

    done

--- client/src/pages/PrivacyPolicy.jsx
+++ client/src/pages/PrivacyPolicy.jsx
@@ -25,7 +25,26 @@ const PrivacyPolicy = () => {
         <p className='text-gray-700 leading-8'>Our Customer Must Have The Authority To Act On Behalf Of The End-User And To Consent To The Collection And Use Of Their Personal Information As Described In This Privacy Policy. If You Are An End-User Using Our Platform, Please Direct Your Privacy Inquiries To Your Administrator. Tech Minds Education Is Not Responsible For The Privacy Or Security Practices Of Our Customers, Which May Differ From Those Set Forth In This Privacy Statement.</p>
         <p className='text-gray-700 mt-5'>We Do Not Send Personalized Advertisement Or Marketing Communication To End Users.</p>
       </div>
-      <div></div>
+      <div className='my-12'>
+        <h3 className='text-xl font-semibold text-[#140342] mb-4'>Use of your Personal Information and Other Data.</h3>
+        <p className='text-gray-700 leading-8'>Tech Minds Education uses this Privacy Policy to govern the use of personal information you provide to us. We use your personal information solely to provide our services to you. We also use your Personal Identifiable Information (PII) together with other Personally Non-Identifiable Information (Non – PII) To Help Us Better Understand Our Users And To Improve The Content And Functionality Of Our Site.</p>
+      </div>
+      <div>
+        <h3 className='text-xl font-semibold text-[#140342] mb-4' >Personal Identifiable Information</h3>
+        <p className='text-gray-700'>We may collect the following personal information from you in the following situations:-</p>
+        <ul className='my-4 list-disc text-gray-700 ml-8'>
+          <li>When You Visit Our Website And Voluntarily Provide The Following Information Via One Of Our Contact Forms, Via A Chat Or Phone Session, Or As Part Of A Purchase Of One Of Our Courses Or Register On Our Site.</li>
+          <li className='my-2'>Contact Information, Such As Name, Email Address, Mailing Address, Phone Number, IP</li>
+          <li className='my-2'>Information about your business, such as company name, company size, business type.</li>
+          <li>Your account details which may include billing information and the account password.</li>
+        </ul>
+        <p className='text-gray-700'>Note: All payment transactions are processed through secure payment gateway providers. We do not store any card information (other than the last 4 digits of your card) in our servers.</p>
+        <p className='text-gray-600 leading-8 my-5'>
+          Users may, however, visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identifiable information, except that it may prevent them from engaging in certain Site related
+          activities.
+        </p>
+      </div>
+      
     </div>
   )
 }
