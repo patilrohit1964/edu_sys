2b711da5b5c0d5fe47a52a73f37878004ce84fde
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-01 12:41:44 +0530

    done

--- client/src/pages/PrivacyPolicy.jsx
+++ client/src/pages/PrivacyPolicy.jsx
@@ -13,6 +13,19 @@ const PrivacyPolicy = () => {
           information, wherever appropriate and/or mandated under applicable privacy laws.
         </p>
       </div>
+      <div className='my-12'>
+        <h3 className='text-xl text-[#140342] font-semibold mb-4'>Tech Minds Education website.</h3>
+        <p className='text-gray-700'>Website:- www.techmindseducation.com</p>
+        <p className='text-gray-700 mt-5'>Please read this Privacy Policy carefully, as it governs how you use our website or its affiliate products. If you do not agree to this Privacy Policy, please do not use our Services.</p>
+      </div>
+      <div>
+        <h3 className='text-xl font-semibold text-[#140342] mb-4'>Notice to End Users</h3>
+        <p className='text-gray-700 leading-8'>In Some Cases An Administrator May Create An Account In Tech Minds Education On The Behalf Of An “End-User” And May Provide His/Her Information, Including Personal Information. In Such Cases The Administrator Who May Be Your Employer Or a tech minds Education Authorized Training Partner (ATP) Is Our “Customer.” We Collect Information Under The Direction Of Our Customers And Have No Direct Relationship With The End-User Whose Personal Data We Process.</p>
+        <p className='my-5 text-gray-700'>An End-User’s Use Of our website or Platform May Be Subject To The Customer’s Policies, If Any.</p>
+        <p className='text-gray-700 leading-8'>Our Customer Must Have The Authority To Act On Behalf Of The End-User And To Consent To The Collection And Use Of Their Personal Information As Described In This Privacy Policy. If You Are An End-User Using Our Platform, Please Direct Your Privacy Inquiries To Your Administrator. Tech Minds Education Is Not Responsible For The Privacy Or Security Practices Of Our Customers, Which May Differ From Those Set Forth In This Privacy Statement.</p>
+        <p className='text-gray-700 mt-5'>We Do Not Send Personalized Advertisement Or Marketing Communication To End Users.</p>
+      </div>
+      <div></div>
     </div>
   )
 }
