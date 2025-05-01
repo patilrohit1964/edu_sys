91de785159e3d4f357042aa7ef16eb782cea5f7f
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-01 13:15:04 +0530

    done

--- client/src/pages/PrivacyPolicy.jsx
+++ client/src/pages/PrivacyPolicy.jsx
@@ -64,12 +64,53 @@ const PrivacyPolicy = () => {
             you give us explicit permission to do so. Regardless of whether you have registered yourself under DND or DNC or NCPR service, you still authorize us to contact you for the above mentioned purposes.
           </li>
         </ul>
-        <p>
+        <p className='mt-5 text-gray-700'>
           When you provide us with personal information about your contacts we will only use this information for the specific reason for which it is provided.
         </p>
       </div>
+      <div className='my-12'>
+        <h3 className='text-xl font-semibold text-[#140342] mb-4'>Share</h3>
+        <p className='text-gray-700'>If Your Subscription Was Provided By Your Employer or a Tech Minds Education Authorized Training Partner, We Will Share Information, Including Personal Information With Them.</p>
+        <p className='leading-8 text-gray-700 my-5'>We may transfer personal information to our affiliates and sub-contractors that help us provide our Service(s). Transfers to subsequent third parties are covered by the service agreements with our data-processors. We reserve the right to disclose your personal data as required by applicable law, in response to lawful requests by public authorities, including meeting national security or law enforcement requirements and when we believe that disclosure is necessary to protect our rights and/or to comply with a judicial proceeding, court order, or other legal process served on us.</p>
+        <p className='text-gray-700 leading-8'>In the event we go through a business transition, such as a merger or acquisition by another company, or sale of all or a portion of our assets, your user account and personal data will likely be among the assets transferred. We may also disclose your personal information to any other third party with your prior consent.</p>
+      </div>
       <div>
-
+        <h3 className='text-xl font-semibold text-[#140342] mb-4'>Data from Third Parties</h3>
+        <p className='text-gray-700 leading-8'>
+          We also obtain data from third parties. We protect data obtained from third parties according to the practices described in this statement, plus any additional
+          restrictions imposed by the source of the data. These third-party sources vary over time, but have included:
+        </p>
+        <ul className='ml-8 text-gray-700 list-disc my-5'>
+          <li className='mb-2'>Data brokers from which we purchase demographic data to supplement the data we collect.</li>
+          <li className='mb-2'>Social networks when you grant permission to make your data public on these networks including, permission to access your data on one or more of these networks.</li>
+          <li className='mb-2'>An enterprise customer where you are an employee using our services.</li>
+          <li className='mb-2'>Existing or past learners who participate in our referral program</li>
+          <li className='mb-2'>Service providers that help us determine a location based on your IP address in order to customize certain products to your location.</li>
+          <li className='mb-2'>Partners with which we offer co-branded services or engage in joint marketing activities, and</li>
+          <li className='mb-2'>Publicly-available sources such as open government databases or other data in the public domain.</li>
+        </ul>
+        <p className='text-gray-700 leading-8'>We may receive Personal Information about you from other sources like postal mail, telephone or fax. We may add this information to the information we have already collected from you via the Site in order to improve the products and services we provide.</p>
+      </div>
+      <div className='my-12'>
+        <h3 className='text-xl font-semibold text-[#140342] mb-4'>Rights to access, correction and deletion of Personal Information</h3>
+        <p className='leading-8 text-gray-700'>
+          You have certain rights in relation to your Personal Information. You can request access to your Personal Information and confirm that it remains correct and up-to-date and also request that we delete your Personal Information. Please also note that certain Personal Information may be exempt from such requests in certain circumstances, which may include if we need to retain your Personal Information to comply with any legal and regulatory purposes or obligation under
+          applicable laws.
+        </p>
+        <p className='text-gray-700 my-5'><b>Right to access: </b>We acknowledge that you have the right to access your Personal Information at any time. Upon such request to access, we will provide you with reasonable access to the Personal Information we collect about you.</p>
+        <p className='text-gray-700 leading-8'>
+          However, there may be limits to the amount of information we can practically provide you. For example, we may limit an individual’s access to Personal
+          Information where the burden or expense of providing access would be disproportionate to the risks to the individual’s privacy or where doing so would violate others’ rights under the applicable laws.
+        </p>
+        <p className='my-5 text-gray-700 leading-8'>
+          <b>Right to Correction:</b> We acknowledge that you have the right to either edit, correct/amend or update any inaccurate or incomplete Personal Information along with the supporting documentation. We provide access to either edit, correct/amend or update your Personal Information at any time by logging into your account. Even after your edit, correct/amend or update to your Personal Information, we may maintain and store older versions of such personal
+          information within our databases, access logs and other records in order to comply with other routine data audits and for legitimate business or any other legal or regulatory purposes or obligations under the applicable laws.</p>
+        <p className='text-gray-700 leading-8'>Where the processing of your Personal Information is based on your consent, you have the right to withdraw your consent granted for processing of your Personal Information by issuing a written request to the Grievance officer.</p>
+        <p className='text-gray-700 my-5'>You will have the opportunity to edit correct, amend, update or delete your Personal Information or withdraw your consent as follows:</p>
+        <ul>
+          <li>If you have independently signed up for our Services, you may access, update or export your Personal Information included in your online account at any time by logging into your account and going to your profile or by reaching out to us.</li>
+          <li>If You Are Using An Enterprise-Provided Account (Tech Minds Education Authorized Training Partner Or Your Employer) Please Review The Section Notice to End Users.</li>
+        </ul>
       </div>
     </div>
   )
