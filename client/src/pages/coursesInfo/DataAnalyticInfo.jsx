22ee7342acd18e740c4eddea8d2e425200ab8b7a
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-07 00:16:18 +0530

    done

--- client/src/pages/coursesInfo/DataAnalyticInfo.jsx
+++ client/src/pages/coursesInfo/DataAnalyticInfo.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const DataAnalyticInfo = () => {
-  return (
-    <div>
-        sdfsfds
-    </div>
-  )
+    return (
+        <div>
+            <h1 className='font-bold text-3xl py-30 max-w-7xl m-auto'>PG Program in Data Science <br /> With Analytics and AI</h1>
+        </div>
+    )
 }
 
 export default DataAnalyticInfo
