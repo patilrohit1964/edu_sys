9f08bdb66dff8d9aa1f7822fbb17da1e843913ba
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-19 00:19:38 +0530

    done

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
         <div>
-            <div className='flex items-center justify-center my-10'>
-                <img src="./images/about.svg" alt="" />
+            <div className='flex items-center justify-center mt-10 mb-30'>
+                <img src="./images/about.svg" alt="" loading='lazy' draggable={"false"} />
             </div>
-            <div className='flex items-center justify-center my-10'>
-                <img src="./images/au2.svg" alt="" />
+            <div className='flex items-center justify-center'>
+                <img src="./images/au.jpg" alt="" loading='lazy' draggable={"false"} />
+            </div>
+            <div className='flex items-center justify-center my-30'>
+                <img src="./images/au3.svg" alt="" loading='lazy' draggable={"false"} />
+            </div>
+            <div className='flex items-center justify-center'>
+                <img src="./images/au4.svg" alt="" loading='lazy' draggable={"false"} />
+            </div>
+            <div className='flex items-center justify-center mt-30 relative'>
+                <img src="./images/about5.svg" alt="" loading='lazy' draggable={"false"} />
+                <Link to={"/contact"}>
+                    <button className='absolute py-2 text-2xl px-10 bg-purple-500 rounded-lg bottom-12 text-white left-62'>Contact Us</button>
+                </Link>
             </div>
         </div>
     )
